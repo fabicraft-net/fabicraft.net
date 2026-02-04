8ba6382ada3e30080f8fb92538610149929bffca
Fabian Mäkilä <contact@fabianadrian.fi>
2026-02-04 20:14:41 +0200

    update favicon and optimize logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,17 @@ export default defineConfig({
 			title: 'FabiCraft',
 			logo: {
 			  src: './src/assets/logo.png'
-			},
+      },
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/favicon.ico',
+            sizes: '32x32',
+          }
+        }
+      ],
 			social: [
 			  { icon: 'discord', label: 'Discord', href: 'https://discord.gg/TJyAWjztUD' },
 			  { icon: 'github', label: 'GitHub', href: 'https://github.com/fabicraftnet' }
